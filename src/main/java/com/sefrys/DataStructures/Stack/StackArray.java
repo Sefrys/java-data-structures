14e234743ce200a317746f83e54d30ee1ac8d06f
Adonalsium <sefrys@gmail.com>
2018-05-15 11:08:17 +0200

    Add getRemainingSpace and push

--- src/main/java/com/sefrys/DataStructures/Stack/StackArray.java
+++ src/main/java/com/sefrys/DataStructures/Stack/StackArray.java
@@ -7,13 +7,22 @@ package com.sefrys.DataStructures.Stack;
 class StackArray<T> {
 
     private T[] array;
+    private int arrayMaxSize;
+    private int arrayCurrentCapacity = 0;
+    private int top = -1;
 
     @SuppressWarnings("unchecked")
     public StackArray(int arrayMaxSize) {
         this.array = (T[]) new Object[arrayMaxSize];
+        this.arrayMaxSize = arrayMaxSize;
     }
 
     void push(T element) {
+        array[++top] = element;
+        arrayCurrentCapacity++;
+    }
 
+    public int getRemainingSpace() {
+        return arrayMaxSize - arrayCurrentCapacity;
     }
 }
