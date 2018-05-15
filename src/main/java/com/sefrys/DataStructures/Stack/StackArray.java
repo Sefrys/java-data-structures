6fa5b1c1b265065649de8d9cedb495e06a641b14
Adonalsium <sefrys@gmail.com>
2018-05-15 10:59:19 +0200

    Create generic array in constructor

--- src/main/java/com/sefrys/DataStructures/Stack/StackArray.java
+++ src/main/java/com/sefrys/DataStructures/Stack/StackArray.java
@@ -4,5 +4,16 @@ package com.sefrys.DataStructures.Stack;
  * Created by Ireneusz Zagan on 15.05.18, 10:41
  * Contact: sefrys@gmail.com
  */
-public class StackArray {
+class StackArray<T> {
+
+    private T[] array;
+
+    @SuppressWarnings("unchecked")
+    public StackArray(int arrayMaxSize) {
+        this.array = (T[]) new Object[arrayMaxSize];
+    }
+
+    void push(T element) {
+
+    }
 }
