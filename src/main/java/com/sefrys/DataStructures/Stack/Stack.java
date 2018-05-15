801c224cd90601adbd152ca6bae5470b8188b084
Adonalsium <sefrys@gmail.com>
2018-05-15 10:41:26 +0200

    Create stack interface

---
+++ src/main/java/com/sefrys/DataStructures/Stack/Stack.java
@@ -0,0 +1,12 @@
+package com.sefrys.DataStructures.Stack;
+
+/**
+ * Created by Ireneusz Zagan on 15.05.18, 10:33
+ * Contact: sefrys@gmail.com
+ */
+public interface Stack<T>{
+    T pop();
+    T peek();
+    void push(T element);
+    boolean isFull();
+}
